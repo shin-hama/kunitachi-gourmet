c43776420ab596ce2a2b397ca25f55df8b34522c
shin-hama <htysivista@gmail.com>
2022-01-16 16:40:38 +0900

    MAINT: Remove bioi

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
+// import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -27,9 +27,7 @@ const BlogPostTemplate = ({ data, location }) => {
           itemProp="articleBody"
         />
         <hr />
-        <footer>
-          <Bio />
-        </footer>
+        <footer>{/* <Bio /> */}</footer>
       </article>
       <nav className="blog-post-nav">
         <ul
