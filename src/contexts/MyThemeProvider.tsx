5eb4bccec75f2f677cdbc683d764a382a8accef8
shin-hama <htysivista@gmail.com>
2022-01-19 07:01:57 +0900

    FEAT: Make font size of mui typography to be responsive

--- src/contexts/MyThemeProvider.tsx
+++ src/contexts/MyThemeProvider.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@mui/material/styles'
 
-const theme = createTheme({
+let theme = createTheme({
   palette: {
     primary: {
       main: '#f44336',
     },
   },
 })
+
+// responsiveFontSize() 関数で画面サイズに合わせてフォントサイズが動的に変わる設定が付与された Theme が取得可能
+theme = responsiveFontSizes(theme)
 
 export const MyThemeProvider: React.FC = ({ children }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>
